2728a19acfbf6b6531ca8e544de60f27e34e03f6
itsasmallfish <hennigvi0@gmail.com>
2025-12-01 22:44:30 +0100

    added diffrent passoword method to solve level 2 and switched to simulation instead of trying to calculate the passes because i couldnt find the correct formula

--- day01.js
+++ day01.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 const Method = {
-    POINT_COUNTER,
-    PASSING_COUNTER
+    POINT_COUNTER: 'POINT_COUNTER',
+    PASSING_COUNTER: 'PASSING_COUNTER'
 }
 
 const DIAL_SIZE = 100
-const PASSOWORD_METHOD = POINT_COUNTER
+const PASSWORD_METHOD = Method.PASSING_COUNTER
 
 let pointer = 50
 let password_counter = 0
@@ -14,31 +14,43 @@ let password_counter = 0
 function shift(instruction) {
     const direction = instruction[0]
     const amount = parseInt(instruction.slice(1), 10)
-    switch (direction) {
-        case "R":
-            pointer += amount
-            break
-        case "L":
-            pointer -= amount
-            break
+    for (let i = 0; i < amount; i++) {
+        move(direction)
     }
-    pointer = pointer % DIAL_SIZE
-    if (pointer == 0) {
+    if (PASSWORD_METHOD == Method.POINT_COUNTER && pointer == 0) {
         password_counter++
     }
 }
 
+function move(direction) {
+
+    switch (direction) {
+        case "R":
+            pointer += 1
+            break;
+        case "L":
+            pointer -= 1
+            break;
+    }
+    pointer = pointer % DIAL_SIZE
+    if (pointer < 0) {
+        pointer += DIAL_SIZE
+    }
+    if (PASSWORD_METHOD == Method.PASSING_COUNTER && pointer == 0) {
+        password_counter++
+    } 
+}
+
 try {
-    const data = fs.readFileSync("day01_input01.txt", "utf8");
-    const instructions = data.split("\n");
+    const data = fs.readFileSync("day01_input01.txt", "utf8")
+    const instructions = data.split("\n")
     for (const instruction of instructions) {
-        // Ensure we don't process empty lines
         if (instruction) {
-            shift(instruction);
+            shift(instruction)
         }
     }
 } catch (error) {
-    console.error("Error reading the file:", error);
+    console.error("Error reading the file:", error)
 }
 
 console.log(password_counter)
